548b01f9256332232b5136ea218f79a6d7461c68
Ryan Kelley <rskelley9@gmail.com>
2013-10-27 22:24:02 -0500

    .

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
-
+  def index
+    @posts = Post.all
+  end
 
 #comes from the posts#new
 def new
@@ -10,15 +12,35 @@ end
 def create
 
   @post = Post.create(post_params)
-  redirect_to @post
+
+  if @post.valid?
+    redirect_to @post
+  else
+    render 'new'
 
    # feedback = params[:content]
    # title = params[:title]
    #  render text: params[:post].inspect
  end
+end
 
- def show
+def show
   @post = Post.find(params[:id])
+end
+
+def edit
+  @post = Post.find(params[:id])
+end
+
+def update
+  @post = Post.find(params[:id])
+
+  if @post.update(params[:post].permit(:title, :content))
+    redirect_to @post
+  else
+    render 'edit'
+    @errors = ["Error, you gone made mm'steak.", "Error you a silly.", "Error I'm a robot", "Error the dog says woof.", "Error you a turd."]
+  end
 end
 
 private
